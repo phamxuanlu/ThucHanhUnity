c5753be439e4ab3b27743690ee5f76f3f695d740
Lu Pham <phamxuanlu@outlook.com>
2018-11-26 22:45:56 +0700

    Animator State Machine, character animation

--- Assets/Scripts/Parallax/Platformer2DCustomController.cs
+++ Assets/Scripts/Parallax/Platformer2DCustomController.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Platformer2DCustomController : MonoBehaviour {
+
+
+    private int score;
+
+    //UI
+    public GameObject scoreLabel;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +20,10 @@ public class Platformer2DCustomController : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void AddCoins(int quantity)
+    {
+        score += quantity;
+        scoreLabel.GetComponent<Text>().text = string.Format("Score: {0}", score);
+    }
 }
